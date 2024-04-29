167244fdcdbb917bdf56acd4f57193af8fa38b15
post-coder <gabrielspanu.dev@gmail.com>
2024-04-29 10:45:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+
+// importiamo axios
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+
+      posts: [],
+
+      
+
+      baseApiUrl: 'http://127.0.0.1:8000/api',
+
+    }
+  },
+
+  mounted() {
+    
+    axios.get(this.baseApiUrl + '/posts').then(res => {
+
+      console.log(res);
+
+
+      this.posts = res.data.results;
+
+    })
+
+  },
+
+  methods: {
+
+  },
+}
+
+</script>
+
+<template>
+  <div class="container py-5">
+
+    <h1>Ciao</h1>
+
+    <ul>
+      <li v-for="post in posts">
+        {{ post.title }}
+      </li>
+    </ul>
+
+    
+  </div>
+</template>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+
+// importiamo bootstrap
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
