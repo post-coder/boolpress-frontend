01242fd9dc7fa648cde67d0c94926ea1adaeed19
post-coder <gabrielspanu.dev@gmail.com>
2024-04-30 12:35:31 +0200

    feat: added footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'AppFooter',
+    data() {
+
+    }
+}
+</script>
+
+<template>
+
+
+<footer>
+    <div class="container ">
+        <p>
+            Copyright (c): 2024 Boolean Classe #118
+        </p>
+    </div>
+
+</footer>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+footer {
+
+    border-top: 1px solid #ffffff44;
+
+    padding: 24px 0;
+
+    p {
+        opacity: .7;
+        font-size: .8em;
+    }
+}
+
+</style>
